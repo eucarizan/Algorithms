1f43b06d80adfa2f57d6486ef2a2026927f2ff40
eucarizan <eucarizan@gmail.com>
2023-08-12 20:59:45 +0800

    initial commit

---
+++ MaxPairwiseProduct/src/main/java/Main.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static long getMaxPairwiseProduct(int[] numbers) {
+        int n = numbers.length;
+        Arrays.sort(numbers);
+        long num1 = numbers[0];
+        long num2 = numbers[1];
+
+        if (n > 2) {
+            long num3 = numbers[n - 1];
+            long num4 = numbers[n - 2];
+
+            if (num1 * num2 < num3 * num4) {
+                return num3 * num4;
+            }
+        }
+
+        return num1 * num2;
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.println(getMaxPairwiseProduct(numbers));
+    }
+}
+
+class FastScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    FastScanner(InputStream stream) {
+        try {
+            br = new BufferedReader(new
+                    InputStreamReader(stream));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    String next() {
+        while (st == null || !st.hasMoreTokens()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Ace Your Next Coding Interview by Learning Algorithms through Programming and Puzzle Solving
+
+## Learning Goals
+### Learn essential algorithmic techniques
+Greedy algorithms, divide-and-conquer, dynamic programming, and many other algorithmic approaches.
+
+### Practice writing efficient and reliable code
+Implement many algorithmic programming challenges in a programming language of choice: C++, Java, Python, C, C#, Haskell, Javascript, Kotlin, Ruby, Rust, and Scala. Invested thousands of hours into designing programming challenges and puzzles. The mantra is "Learning Algorithms Through Programming and Puzzle Solving" rather than the "Learning Algorithms by Answering Multiple Choice Questions" approach often used in MOOCs.
+
+### Prepare for a coding interview
+No other online course in Algorithms focuses on programming challenges that can be encountered at coding interviews.
